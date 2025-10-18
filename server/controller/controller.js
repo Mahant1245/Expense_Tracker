066fa175089802c60020cc80cd7ee2c5b1b4b46b
Mahant1245 <mahant1245@gmail.com>
2025-10-18 22:10:30 +0100

    added get post and delete request for backend data. Redux remaining

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,20 +1,113 @@
 // a file to manage and access all controllers
 
-const model=require('../models/model')
+const model = require('../models/model');
 
-// post request on: http://localhost:8080/api/categories
-function create_Categories(req,res ){
+// POST request on: http://localhost:8080/api/categories
+async function create_Categories(req, res) {
+  try {
     const Create = new model.Categories({
-        type:"Savings",
-        color:'#1f3b5c'
+      type: "Investment",
+      color: "#FCBE44"
+    });
+
+    // Save using async/await instead of callback
+    const savedCategory = await Create.save();
+
+    // Return the saved data as JSON
+    return res.json(savedCategory);
+
+  } catch (err) {
+    console.error(" Error while creating category:", err);
+    return res
+      .status(400)
+      .json({ message: `Error while creating categories: ${err.message}` });
+  }
+}
+
+// get request on: http://localhost:8080/api/categories
+async function get_Categories(req, res) {
+    let data=await model.Categories.find({})
+    // filter var only for type and color
+    let filter=await data.map(v=> Object.assign({},{type:v.type,color:v.color}));
+
+    return res.json(filter)
+}
+
+
+// post request on: http://localhost:8080/api/transaction
+// controller for creating a new transaction
+async function create_transation(req, res) {
+  try {
+    if (!req.body)
+      return res.status(400).json("Post HTTP data not provided");
+
+    const { name, type, amount } = req.body;
+
+    const create = new model.Transactions({
+      name,
+      type,
+      amount,
+      date: new Date()
+    });
+
+    const savedTransaction = await create.save();
+    return res.json(savedTransaction);
+
+  } catch (err) {
+    console.error("Error while creating transaction:", err);
+    return res
+      .status(400)
+      .json({ message: `Error while creating a transaction: ${err.message}` });
+  }
+}
+
+
+// get request on: http://localhost:8080/api/transaction
+async function get_transaction(req, res) {
+    let data=await model.Transactions.find({});
+
+    return res.json(data)
+}
+
+// delete request on: http://localhost:8080/api/transaction
+async function delete_transaction(req,res) {
+    if(!req.body) res.status(400).json({message:"REquest body not found"});
+    // await instead of callback
+  const result = await model.Transactions.deleteOne(req.body)
+    .then(() => res.json("Record Deleted!"))
+    .catch((err) => res.status(400).json({ message: "Error while deleting transaction record", error: err.message }));
+}
+
+// get request on: http://localhost:8080/api/labels
+async function get_labels(req, res) {
+    model.Transactions.aggregate([
+        {
+            // this allow us to lookup specific field from different collections
+            $lookup:{
+                from:"categories",
+                localField:'type',//specifies the field we want to join by the local connevtion
+                foreignField:'type',//specifies the field we want to join by the foriegn connevtion
+                as:"categories_info"//name of ouput array
+            }
+        },
+        {
+            $unwind:"$categories_info"
+        }
+    ]).then(result =>{
+        let data =result.map(v=>Object.assign({},{_id:v._id,name:v.name,type:v.type,amount:v.amount,color:v.categories_info['color']}));
+        res.json(data);
+    }).catch(error=>{
+        res.status(400).json("Lookup Collection Error");
     })
 
-    Create.save(function(err){
-        if(!err) return res.json(Create)
-        return res.status(400).json({message:`Error while creating categories ${err}`});
-    });
 }
 
-module.exports={
-    create_Categories
-}
+
+module.exports = {
+  create_Categories,
+  get_Categories,
+  create_transation,
+  get_transaction,
+  delete_transaction,
+  get_labels
+};
--- server/routes/route.js
+++ server/routes/route.js
@@ -4,9 +4,18 @@ const controller = require('../controller/controller');
 const routes = require('express').Router();
 
 //the endpoint here is categories
-routes.route('/api/categories')
-
 // calls the create_Categories function from controller.js
-.post(controller.create_Categories);
+
+routes.route('/api/categories')
+    .post(controller.create_Categories)
+    .get(controller.get_Categories);
+
+routes.route('/api/transaction')
+    .post(controller.create_transation)
+    .get(controller.get_transaction)
+    .delete(controller.delete_transaction);
+
+routes.route('/api/labels')
+    .get(controller.get_labels);
 
 module.exports=routes;
